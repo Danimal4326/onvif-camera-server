b4fbad9ebf2e089df5e7f2ec9856ac6b36bbaea3
Dan Chlopek <1285500+Danimal4326@users.noreply.github.com>
2024-09-24 00:58:44 -0500

    fixing streaming issue

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -7,7 +7,7 @@ HEIGHT="${HEIGHT:-720}"
 MANUFACTURER="${MANUFACTURER:-Onvif}"
 MODEL="${MODEL:-Camera}"
 SERIAL="${SERIAL:-00:01:02:03:04:05}"
-HWID="${HWID:-HW_ID}"
+HWID="${HWID:-1234}"
 FW_VER="${FW_VER:-1.0.0}"
 TYPE="${TYPE:-H264}"
 
@@ -38,7 +38,7 @@ fi
 echo "Starting wsdd daemon"
 /onvif-camera-server/wsdd \
   --if_name ${INTERFACE} \
-  --type tdn:NetworkVideoTransmitter \
+  --type  "tdn:NetworkVideoTransmitter" \
   --xaddr "http://%s:1000/onvif/device_service" \
   --scope "onvif://www.onvif.org/name/${MANUFACTURER}-${MODEL} \
            onvif://www.onvif.org/Profile/Streaming  \
@@ -47,23 +47,23 @@ echo "Starting wsdd daemon"
 
 echo "Starting onvif_srvd daemon"
 /onvif-camera-server/onvif_srvd \
-  --ifs ${INTERFACE} \
-  --scope onvif://www.onvif.org/name/${MANUFACTURER}-${MODEL} \
-  --scope onvif://www.onvif.org/hardware/S_HARDWARE \
-  --scope onvif://www.onvif.org/hardware/S_LOCATION \
-  --scope onvif://www.onvif.org/Profile/S  \
-  --scope onvif://www.onvif.org/Profile/Streaming \
-  --name RTSP \
-  --width ${WIDTH} \
-  --height ${HEIGHT} \
-  --url ${URL} \
-  --snapurl ${SNAPSHOT_URL} \
-  --model ${MODEL} \
-  --manufacturer ${MANUFACTURER} \
-  --serial_num ${SERIAL} \
-  --hardware_id ${HWID} \
-  --firmware_ver ${FW_VER} \
-  --type ${TYPE}
+  --ifs "${INTERFACE}" \
+  --scope "onvif://www.onvif.org/name/${MANUFACTURER}-${MODEL}" \
+  --scope "onvif://www.onvif.org/hardware/S_HARDWARE" \
+  --scope "onvif://www.onvif.org/hardware/S_LOCATION" \
+  --scope "onvif://www.onvif.org/Profile/S"  \
+  --scope "onvif://www.onvif.org/Profile/Streaming" \
+  --name         "RTSP" \
+  --width        "${WIDTH}" \
+  --height       "${HEIGHT}" \
+  --url          "${URL}" \
+  --snapurl      "${SNAPSHOT_URL}" \
+  --model        "${MODEL}" \
+  --manufacturer "${MANUFACTURER}" \
+  --serial_num   "${SERIAL}" \
+  --hardware_id  "${HWID}" \
+  --firmware_ver "${FW_VER}" \
+  --type         "${TYPE}"
 
 while true; do sleep 1000; done &
 wait
